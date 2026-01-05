b78cb845b2361dc243eda08040723b186739a201
shrisashtitours <shrisashtitours@gmail.com>
2026-01-05 15:32:40 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log(Website Loaded Successfully);
+function sendMessage() {
+  let name = document.getElementById("name").value;
+  let phone = document.getElementById("phone").value;
+  let message = document.getElementById("message").value;
+
+  let url = "https://wa.me/919566749964?text="
+    + encodeURIComponent(
+      "Name: " + name +
+      "\nPhone: " + phone +
+      "\nMessage: " + message
+    );
+
+  window.open(url, "_blank");
+}
